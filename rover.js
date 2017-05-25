903fa32ee73388f0650fc6de7528674949846c7c
valzie <skyla@users.noreply.github.com>
2017-05-25 14:31:50 -0400

    Iteration2-Part1

--- rover.js
+++ rover.js
@@ -4,9 +4,81 @@ var rover = {
   rightOrLeft: "L",//using R or L
   backwardForward: "B"// using B or F
 };
+//ITERATION 2 (first part) BELOW: REFERS TO ROVER OBJECT ABOVE *****************
+function goForward(arr){
+  for(var i=0;i<arr.length;i++){
+    if(rover.direction ==="N"){
+      switch(arr[i]){
+      case 'F':
+      rover.position[1]++;
+      break;
+      case 'B':
+      rover.position[1]-- ;
+      break;
+      case "R":
+      rover.direction = "E";
+      break;
+      case "L":
+      rover.direction = "W";
+      break;
+      }//closes switch
+    }//closes if
+    else if(rover.direction ==="E"){
+      switch(arr[i]){
+        case 'F':
+        rover.position[0]++;
+        break;
+        case 'B':
+        rover.position[0]--;
+        break
+        case "R":
+        rover.direction = "S";
+        break;
+        case "L":
+        rover.direction = "N";
+        break;
+      }//closes switch
+    }//closes if
+    else if(rover.direction ==="S"){
+      switch(arr[i]){
+        case 'F':
+        rover.position[1]--;
+        break;
+        case 'B':
+        rover.position[1]++;
+        break;
+        case "R":
+        rover.direction = "W";
+        break;
+        case "L":
+        rover.direction = "E";
+        break;
+      }//closes switch
+    }//closes if
+    else if(rover.direction ==="W"){
+      switch(arr[i]){
+        case 'F':
+        rover.position[0]--;
+        break;
+       case 'B':
+        rover.position[0]++;
+        break;
+        case "R":
+        rover.direction = "N";
+        break;
+        case "L":
+        rover.direction = "S";
+        break;
+    }//closes switch
+  }//closes if
+  }//closes the loop
+  return ["Ending Position:  " + rover.position, "Ending Direction:  " + rover.direction];
+}//closes function
+goForward(["F","B","L","F"]);//["F","F","R","L"] // expecting result of 4,4
+
 
 //ITERATION 1 BELOW: REFERS TO ROVER OBJECT ABOVE *****************************
-
+/*
 function backwardForward(){
 if(rover.direction ==="N"){
   switch(rover.backwardForward){
@@ -101,3 +173,4 @@ function rightOrLeft(){//result updates the rover.direction it is facing;
 rightOrLeft();
 // console.log("AfterRunFunction",rover.direction);
 console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]" + ", New Rover Direction: [" + rover.direction  + "]");
+*/
